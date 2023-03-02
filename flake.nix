1c17155512a0f98b79d9c09af161f88797beb10b
Splinter Suidman <splinter@mannenopdemaan.nl>
2023-03-02 11:55:41 +0100

    Add Nix files

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Modellenpracticum";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = nixpkgs.legacyPackages.${system};
+    pythonPackages = pkgs.python3Packages;
+    python = pythonPackages.python;
+  in {
+    packages.default = pkgs.gdal;
+    devShell = pkgs.mkShell {
+      buildInputs = [
+        pkgs.gdal
+        pkgs.qgis
+        (python.withPackages (ps:
+        with ps; [
+          python-lsp-server
+          autopep8
+          flake8
+          pylint
+          black
+          geopandas
+        ]))
+      ];
+    };
+  });
+}
