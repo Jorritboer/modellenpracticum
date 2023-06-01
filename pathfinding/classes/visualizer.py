9d9ef730b66e7d4c80620cf768f49e82cbf67883
Jorrit de Boer <jorritdb@hotmail.com>
2023-06-01 13:40:25 +0200

    Add path_through_same_cost

--- pathfinding/classes/visualizer.py
+++ pathfinding/classes/visualizer.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from random import randint
 import json
 import os
+import math
 from typing import Tuple
 from ..constants.paths import GEOJSON_DATA_PATH
 from ..classes import Grid
@@ -24,7 +25,76 @@ class Visualizer:
         path = path.copy()
 
         def path_through_same_cost(p1: Tuple[int, int], p2: Tuple[int, int]):
-            return randint(0, 8) != 0
+            """Perform a discrete raytrace through all points, checking if their costs are the same."""
+            (x1, y1) = p1
+            (x2, y2) = p2
+
+            # When the lines are axis-aligned, we must use a different approach
+            if x1 == x2:
+                cost = self.grid.get_cost((x1, max(y1, y2)))
+                return all(
+                    [
+                        self.grid.get_cost((x1, y)) == cost
+                        for y in range(min(y1, y2), max(y1, y2))
+                    ]
+                )
+            if y1 == y2:
+                cost = self.grid.get_cost((max(x1, x2), y1))
+                return all(
+                    [
+                        self.grid.get_cost((x, y1)) == cost
+                        for x in range(min(x1, x2), max(x1, x2))
+                    ]
+                )
+
+            # The approach here is to check the grid-elements the line goes through for each y-coordinate individually.
+            # The slope of the line indicates how many grid-elements we can go through before moving up/down into the next layer.
+
+            slope = abs((y2 - y1) / (x2 - x1))
+            x_sign = 1 if x2 - x1 > 0 else -1
+            y_sign = 1 if y2 - y1 > 0 else -1
+            e = 0.1  # All grid elements e away from the line should be considered
+            cost = self.grid.get_cost(p2)
+
+            x = x1 + 0.5
+            y = y1
+
+            # Follow the line through the layer for dx steps,
+            # checking each element (possibly including some border points e away from the line)
+            def check_layer(dx, x, y):
+                if self.grid.get_cost((math.floor(x - x_sign * e), y)) != cost:
+                    return False, None, None
+                if self.grid.get_cost((math.floor(x), y)) != cost:
+                    return False, None, None
+                while dx > 0:
+                    if dx >= 1:
+                        x += x_sign
+                        dx -= 1
+                    else:
+                        x += x_sign * dx
+                        dx = 0
+                    if self.grid.get_cost((math.floor(x), y)) != cost:
+                        return False, None, None
+                if self.grid.get_cost((math.floor(x + x_sign * e), y)) != cost:
+                    return False, None, None
+                y += y_sign
+                return True, x, y
+
+            # we start at the middle of the first tile, so the first dy is .5
+            success, x, y = check_layer(0.5 / slope, x, y)
+            if not success:
+                return False
+            # afterwards it's constantly 1
+            for _ in range(abs(y2 - y1) - 1):
+                success, x, y = check_layer(1 / slope, x, y)
+                if not success:
+                    return False
+            # and .5 again for the final step
+            success = check_layer(0.5 / slope, x, y)
+            if not success:
+                return False
+
+            return True
 
         checkPoint = 0
         currentPoint = 1
